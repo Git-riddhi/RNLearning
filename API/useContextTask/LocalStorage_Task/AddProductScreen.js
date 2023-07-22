bfdcaa411d869b8dbaef61f4d8c9c8013255b078
HBian-riddhi <riddhi.patel@hiddenbrains.in>
2023-07-22 19:22:25 +0530

    Login-Signup Flow with functionality

---
+++ API/useContextTask/LocalStorage_Task/AddProductScreen.js
@@ -0,0 +1,189 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, ImageBackground } from 'react-native';
+import { View, StyleSheet, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import { event } from 'react-native-reanimated';
+
+// const deviceWidth = Dimensions.get("screen").width
+// const deviceHeight = Dimensions.get("screen").height
+
+const AddProductScreen = (props) => {
+    const [name, setName] = useState('')
+    const [id, setId] = useState('')
+    const [address, setAddress] = useState('')
+
+    const storeProductData = async () => {
+
+        const productData = {
+            'Name': name, 'ID': id, 'Address': address
+        }
+        console.log('productData===>', productData);
+
+        try {
+            const productDataArray = [productData]
+            // const existingValue = await getTransaction(productData);
+            // console.log("existingTransactions-Data ===>",existingValue);
+            const oldProductData = await AsyncStorage.getItem('ProductKey')
+
+            const oldData = JSON.parse(oldProductData)
+            console.log("oldProductData======", oldData)
+            if (oldData !== null) {
+                const newDataArray =  [...oldData,...productDataArray]
+                console.log("newDataArray======",newDataArray)
+                await AsyncStorage.setItem('ProductKey', JSON.stringify(newDataArray))
+                props.navigation.navigate('Home')
+                // props.route.params.refreshFun
+
+            } else {
+                await AsyncStorage.setItem("ProductKey", JSON.stringify(productDataArray));
+                props.navigation.navigate('Home')
+            }
+
+        } catch (err) {
+            console.log('error ===>', err);
+        }
+    };
+
+    useEffect(() => {
+        if (
+            props.route.params.item) {
+            // console.log("previous item", props.route.params.item)
+            setName(props.route.params.item.Name)
+            // console.log("previous item's name :", props.route.params.item.Name)
+            setId(props.route.params.item.ID)
+
+        }
+
+    }, [])
+
+    // const itemEvent = () => {
+    //     let event = { 'Name': name, 'ID': id, 'Task': task, 'TaskId': taskId }
+    //     props.route.params.event(event)
+
+    //     props.navigation.goBack()
+
+    //     // console.log("event", event)
+
+    // }
+
+    // const getUpdateData = () => {
+    //     let updatedata = { 'Name': name, 'ID': id, 'Task': task, 'TaskId': taskId }
+    //     props.route.params.updateEvent(updatedata)
+
+    //     props.navigation.goBack()
+
+    //     console.log("updated data :", updatedata)
+    // }
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                source={require("../../../assets/bg.png")}
+                resizeMode="cover"
+                style={styles.ImageBackground}
+            >
+                <Text style={styles.heading}>ADD ITEMS</Text>
+                <View style={styles.firstView}>
+
+                    <TextInput
+                        placeholder="Enter ID"
+                        onChangeText={(id) => setId(id)}
+                        style={styles.textInputStyle}
+                        autoCapitalize="words"
+                        placeholderTextColor='grey'
+                        value={id}
+
+                    />
+                    <TextInput
+                        placeholder="Enter Name"
+                        onChangeText={(name) => setName(name)}
+                        style={styles.textInputStyle}
+                        autoCapitalize="words"
+                        placeholderTextColor='grey'
+                        value={name}
+                    />
+                    <TextInput
+                        placeholder="Enter address"
+                        numberOfLines={2}
+                        onChangeText={(address) => setAddress(address)}
+                        style={styles.textInputStyle}
+                        autoCapitalize="words"
+                        placeholderTextColor='grey'
+                        value={address}
+                    />
+
+                    {/* <TouchableOpacity
+                        style={styles.submitButton}
+                        onPress={() => { storeProductData() }}>
+                        <Text style={styles.submitButtonText}> SUBMIT </Text>
+                    </TouchableOpacity> */}
+
+
+                    {props.route.params.item
+                        ? <TouchableOpacity
+                            style={styles.submitButton}
+                            onPress={() => { getUpdateData() }}>
+                            <Text style={styles.submitButtonText}> UPDATE </Text>
+                        </TouchableOpacity>
+                        : <TouchableOpacity
+                            style={styles.submitButton}
+                            onPress={() => { storeProductData() }}>
+                            <Text style={styles.submitButtonText}> SUBMIT </Text>
+                        </TouchableOpacity>
+                    }
+                </View>
+            </ImageBackground>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'orange',
+    },
+    ImageBackground: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    heading: {
+        color: '#1e90ff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 25,
+        marginTop: 30,
+        marginBottom: 10,
+        marginLeft: 25,
+    },
+    image: {
+        height: 50,
+        width: 50,
+        tintColor: 'white'
+    },
+    firstView: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInputStyle: {
+        width: '80%',
+        borderRadius: 20,
+        backgroundColor: '#f0ffff',
+        padding: 7,
+        elevation: 5,
+        // marginRight: 20,
+        marginTop: 30
+    },
+    submitButton: {
+        backgroundColor: 'orange',
+        padding: 10,
+        marginTop: 50,
+        borderRadius: 20,
+    },
+    submitButtonText: {
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: 'bold'
+    }
+});
+export default AddProductScreen;
---
+++ API/useContextTask/LocalStorage_Task/SplashScreen.js
@@ -0,0 +1,50 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useContext, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const SplashScreen = (props) => {
+
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const getItem = await AsyncStorage.getItem('Registerkey')
+        if (getItem === null) {
+
+            console.log("yes null");
+
+            setTimeout(() => {
+                props.navigation.navigate("Login")
+            }, 1000);
+        }
+        else {
+            setTimeout(() => {
+                props.navigation.navigate("Home")
+            }, 1000);
+            // console.log("splash get item ==else==>", getItem);
+
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>SPLASH</Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+        color:'orange',
+        fontWeight: 'bold'
+    }
+})
+export default SplashScreen;
