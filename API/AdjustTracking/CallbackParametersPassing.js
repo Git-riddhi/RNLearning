ff27b348dd0bfddcb4af7f8dd72a15e9bbb8805c
HBian-riddhi <riddhi.patel@hiddenbrains.in>
2023-08-19 11:23:50 +0530

    callback parameters pass in trackevent

---
+++ API/AdjustTracking/CallbackParametersPassing.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+import { Adjust, AdjustEvent } from 'react-native-adjust';
+
+
+const CallbackParametersPassing = () => {
+
+  const trackEventWithParameters = (eventName) => {
+    var adjustEvent = new AdjustEvent(eventName);
+    const callbackParameters = {
+      key1: 'value1',
+      key2: 'value2',
+      key3: 'value3',
+      key4: 'value4',
+      key5: 'value5',
+    };
+
+    for (const key in callbackParameters) {
+      // if (callbackParameters.hasOwnProperty(key)) {
+      const value = callbackParameters[key];
+      adjustEvent.addCallbackParameter(key, value);
+      // }
+    }
+    console.log('adjustEvent', adjustEvent);
+    Adjust.trackEvent(adjustEvent);
+  };
+
+  return (
+    <View>
+      <Button title="Track Event" onPress={() => { trackEventWithParameters("abc123") }} />
+    </View>
+  );
+};
+
+export default CallbackParametersPassing;
